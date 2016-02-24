216ff62e4ea27bc99b95da89bcb48d8226e4e3e2
kassergey <kassergey@mail.ru>
2016-02-24 14:18:43 +0200

    Add ability to work from Terminal
    Add ability to work from Terminal and using mongoose not only with
    express.js

---
+++ bin/startvocab.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+var mongoose = require('mongoose');
+var Todo = require('../models/Todo.js');
+mongoose.connect('mongodb://localhost/Vocab');
+var db = mongoose.connection;
+
+//vocab --add wordOrigin wordTranslation
+if(process.argv.length == 2)
+{
+    console.log("Use --add word translation, if you want add new entry.\n" + 
+                "write simply word after this command for translation.");
+}
+else if(process.argv[2] == '--add')
+{
+    db.once('open', function () {
+        console.log("Creating");
+        Todo.create({wordOrigin:process.argv[3], wordTranslation:process.argv[4]}, function (e, body) {
+            console.log(body);
+            console.log("Created");
+        });
+    });
+}
+else{
+    db.once('open', function () {
+        Todo.findOne({wordOrigin: process.argv[2]}, 'wordOrigin wordTranslation',function (e, body) {
+            if(body == null)
+            {
+                console.log('Word not found.');
+            }
+            else
+            {
+                console.log(body.wordOrigin + ' - ' + body.wordTranslation);
+            }
+        });
+    });
+}

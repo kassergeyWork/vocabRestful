68dd6212264b9e38defe9c60d5615a3fc0e65ce4
kassergey <kassergey@mail.ru>
2016-03-08 22:45:13 +0200

    Refactoring
    Replace cbFunc from each function in routes\vocab.js with
    runJsonLikeAMemberFunctionOfObjectRes.

--- routes/vocab.js
+++ routes/vocab.js
@@ -3,51 +3,35 @@ var router = express.Router();
 
 var db = require('../lib/DBVocab');
 
-
+//it's needed, i can describe later why i made that
+var runJsonLikeAMemberFunctionOfObjectRes = function(res)
+{
+    return function(response){ res.json(response);};
+};
 
 /* GET /WordTranslations listing. */
 router.get('/', function(req, res, next) {
-    var cbFunc = function(responce)
-    {
-        res.json(responce);
-    };
-    db.getAllRecords(cbFunc);
+    db.getAllRecords(runJsonLikeAMemberFunctionOfObjectRes(res));
 });
 
 /* POST /WordTranslations */
 router.post('/', function(req, res, next) {
-    var cbFunc = function(responce)
-    {
-        res.json(responce);
-    };
-    db.addWord(req.body.wordOrigin, req.body.wordTranslation, cbFunc);
+    db.addWord(req.body.wordOrigin, req.body.wordTranslation, runJsonLikeAMemberFunctionOfObjectRes(res));
 });
 
 /* GET /WordTranslations/id */
 router.get('/:id', function(req, res, next) {
-  	var cbFunc = function(responce)
-    {
-        res.json(responce);
-    };
   	db.getWordById(req.params.id, cbFunc);
 });
 
 /* PUT /WordTranslations/:id */
 router.put('/:id', function(req, res, next) {
-  	var cbFunc = function(responce)
-    {
-        res.json(responce);
-    };
-  	db.getWordByIdAndUpdate(req.params.id, req.body, cbFunc);
+  	db.getWordByIdAndUpdate(req.params.id, req.body, runJsonLikeAMemberFunctionOfObjectRes(res));
 });
 
 /* DELETE /WordTranslations/:id */
 router.delete('/:id', function(req, res, next) {
-  	var cbFunc = function(responce)
-    {
-        res.json(responce);
-    };
-  	db.getWordByIdAndRemove(req.params.id, req.body, cbFunc);
+  	db.getWordByIdAndRemove(req.params.id, req.body, runJsonLikeAMemberFunctionOfObjectRes(res));
 });
 
 module.exports = router;
